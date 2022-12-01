341f361dc4eb98282050a0aa25fb676205c58876
Nikolay Dutskinov <niki.dutskinov@hotmail.com>
2022-12-01 17:28:24 +0200

    first commit

---
+++ SparkleTest/SparkleTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SparkleTest
+//
+//  Created by Nikolay N. Dutskinov on 1.12.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
